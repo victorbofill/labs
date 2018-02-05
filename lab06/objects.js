cbfd379c4d3a4facf9292d6ff670da8082b3d38d
Victor Bofill <terranvas@gmail.com>
2018-02-05 11:05:54 -0800

    Completed objects.js

--- lab06/objects.js
+++ lab06/objects.js
@@ -1,8 +1,8 @@
-/*
-Creating objects 
-*/
+// /*
+// Creating objects 
+// */
 
-// properties
+// // properties
 const song = {
   title: 'Row Row',
   lyrics: 'row row row your boat gently down the stream',
@@ -17,7 +17,7 @@ const songTwo = {
   isFavorite: true
 };
 
-// methods
+// // methods
 const thirdSong = {
   title: 'Free Bird',
   lyrics: 'a bunch of stuff',
@@ -29,7 +29,7 @@ const thirdSong = {
   }
 }
 
-/* Accessing properties and methods */
+// /* Accessing properties and methods */
 const songs = ['one two three', 'blue cows', 'remote control'];
 console.log(songs[1]); //'blue cows'
 
@@ -39,11 +39,11 @@ console.log((thirdSong['title']));
 thirdSong.title;
 console.log(thirdSong.title);
 
-// Changing properties
+// // Changing properties
 thirdSong.lyrics = 'lyrics lyrics lyrics';
 console.log(thirdSong.lyrics);
 
-// Executing methods
+// // Executing methods
 thirdSong.sing();
 
 /* Accessing its own properties with the all dreaded *this* */
@@ -55,6 +55,49 @@ thirdSong.sing();
 */
 /* referencing existing html elements  */
 
+console.log(document);
+
+console.log(document.getElementById('notes'));
+
+const notes = document.getElementById('notes');
+console.log(notes);
+
+const h1 = document.getElementsByTagName('h1');
+console.log(h1);
+
+const surprise = document.getElementsByClassName('surprise');
+console.log(surprise);
+
+const h1Again = document.querySelector('h1');
+console.log(h1Again);
+
+const surprisesAgain = document.querySelectorAll('.surprise');
+console.log(surprisesAgain);
+
+h1Again.id = 'something';
+h1Again.classList.add('surprise');
+
+h1Again.style.color = 'red';
 
 /* creating new elements */
 // adding them to the DOM
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'floo fla flee';
+console.log(newParagraph);
+
+const newImageEle = document.createElement('img');
+console.log(newImageEle);
+newImageEle.src = '#';
+// newImageEle.alt = 'test';
+newImageEle.setAttribute('alt', 'testing 123');
+let imgAttribute = newImageEle.getAttribute('src');
+console.log('image src attribute: ' + imgAttribute);
+
+newImageEle.removeAttribute('src');
+imgAttribute = newImageEle.getAttribute('src');
+console.log('image src attribute: ' + imgAttribute);
+
+// appendChild, called on an element
+notes.appendChild(newParagraph);
+newParagraph.appendChild(newImageEle);
